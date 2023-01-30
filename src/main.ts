c9d47d355478afdcf12996a2dd01cffc730fa283
Thiago Borges <thiago@borges.pro>
2023-01-30 16:58:20 -0300

    updating script

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,34 @@
-import * as core from '@actions/core';
-import { getOctokit, context } from '@actions/github';
+import * as core from '@actions/core'
+import {context, getOctokit} from '@actions/github'
 
-import { createBranch } from './create-branch';
+import {createBranch} from './create-branch'
 
 async function run() {
   try {
-    const prefix = core.getInput('prefix');
-    const branch = core.getInput('branch');
-    const sha = core.getInput('sha');
-    const squads = core.getMultilineInput('squads');
+    const prefix = core.getInput('prefix')
+    const branch = core.getInput('branch')
+    const sha = core.getInput('sha')
+    const squads = core.getMultilineInput('squads')
 
-    const currentVersion = branch.replace(/[a-z|-]/g, '');
+    const currentVersion = branch.replace(/[a-z|-]/g, '')
 
-    let areCreated = false;
+    let areCreated = false
 
     for (const squad of squads) {
-      core.debug(`Creating branch ${branch}`);
-      const newBranch = `${prefix}-${squad}-${currentVersion}`.toLowerCase();
-      areCreated = areCreated || !!(await createBranch(getOctokit, context, newBranch, sha));
+      core.debug(`Creating branch ${branch}`)
+      const newBranch = `${prefix}-${squad}-${currentVersion}`.toLowerCase()
+      const created = !!(await createBranch(
+        getOctokit,
+        context,
+        newBranch,
+        sha
+      ))
+      areCreated = areCreated || created
     }
 
-    core.setOutput('created', Boolean(areCreated));
+    core.setOutput('created', Boolean(areCreated))
   } catch (error: any) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
 }
-run();
+run()
